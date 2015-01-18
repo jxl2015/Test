8e5d33477e8e786f851572cc0f34c9d8b1ccc566
jxl2015 <jxl828@yahoo.com>
2015-01-18 12:01:24 -0500

    Simple test
    Simple VS 2005 C++ sample to test GitHub.  Initial code.

---
+++ Hello/Hello.cpp
@@ -0,0 +1,12 @@
+// Hello.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	std::cout << "Hello world!\n";
+	return 0;
+}
+
